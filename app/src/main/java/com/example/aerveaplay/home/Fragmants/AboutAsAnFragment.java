e58e3a3a248dbf6115e3784937e85dd9f1ac0a87
rmdaniii <ahmedtramadan4@gmail.com>
2023-07-25 22:58:50 +0300

    Adding a Fragments Sliding Navigation Menu

---
+++ app/src/main/java/com/example/aerveaplay/home/Fragmants/AboutAsAnFragment.java
@@ -0,0 +1,35 @@
+package com.example.aerveaplay.home.Fragmants;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.aerveaplay.R;
+
+
+public class AboutAsAnFragment extends Fragment {
+
+
+
+    public AboutAsAnFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_as_an, container, false);
+    }
+}
