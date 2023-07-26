bd1728a0da0ce9d449820878370e44e54b184ab6
rmdaniii <ahmedtramadan4@gmail.com>
2023-07-27 01:20:57 +0300

    Some Updates

--- app/src/main/java/com/example/aerveaplay/Login/SplashActivity.java
+++ app/src/main/java/com/example/aerveaplay/Login/SplashActivity.java
@@ -3,8 +3,10 @@ package com.example.aerveaplay.Login;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -15,9 +17,11 @@ import com.example.aerveaplay.databinding.ActivitySplashBinding;
 import com.example.aerveaplay.onboarding.ui.OnboardingActivity;
 
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     Animation anim;
+    private ActivitySplashBinding  binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +30,18 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        ActivitySplashBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
         anim.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onAnimationStart(Animation animation) {
-            }
+            public void onAnimationStart(Animation animation) { }
             @Override
             public void onAnimationEnd(Animation animation) {
                 startActivity(new Intent(SplashActivity.this, OnboardingActivity.class));
                 finish();
-
             }
             @Override
             public void onAnimationRepeat(Animation animation) {
@@ -45,7 +49,6 @@ public class SplashActivity extends AppCompatActivity {
         });
 
         binding.logoSplashImg.startAnimation(anim);
-
     }
 
     @Override
